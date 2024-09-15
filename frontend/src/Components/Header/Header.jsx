1eb0fe3859db462a4128ddc79be88b8277605357
and <andersonvittor1801@gmail.com>
2024-09-15 11:01:01 -0300

    feat(frontend): creating and styling header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import * as H from './Styles'
+import iconHeader from '../../assets/iconHeader.png'
+import { FaRegUser } from "react-icons/fa";
 
 export const Header = () => {
   return (
     <H.Header>
-        Teste
+        <H.HeaderTitle>
+            <span><img src={iconHeader} alt="icone" /></span>
+            <h2>Clínica de Psicologia Ana Silva</h2>
+        </H.HeaderTitle>
+
+        <H.HeaderIcon>
+            <span><FaRegUser/></span>
+        </H.HeaderIcon>
     </H.Header>
   )
 }
--- frontend/src/Components/Header/Styles.jsx
+++ frontend/src/Components/Header/Styles.jsx
@@ -2,5 +2,37 @@ import styled from "styled-components";
 
 export const Header = styled.header`
     width: 100%;
-    background-color: blue;
+    background-color: #457B9D;
+    height: 62px;
+    display: flex;
+    flex-direction: row;
+`
+
+export const HeaderTitle = styled.div`
+    width: 100%;
+    height: 62px;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    padding: 10px;
+    gap: 5px;
+
+    img {
+        width: 40px;
+    }
+`
+
+export const HeaderIcon = styled.div`
+    width: 100%;
+    height: 62px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 10px;
+
+    span {
+        font-size: 25px;
+        cursor: pointer;
+        color: #fff;
+    }
 `
