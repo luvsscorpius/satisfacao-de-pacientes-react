eae6e2ae60a4fe7af86f12caf87324b67ecbae66
and <andersonvittor1801@gmail.com>
2024-09-15 16:27:02 -0300

    feat(frontend): finshing styling /home

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,32 +1,54 @@
 import React from 'react'
 import * as H from './Styles'
-import { FaUser , FaRegStar } from "react-icons/fa";
+import { FaUser, FaRegStar } from "react-icons/fa";
 import { BsSend } from "react-icons/bs";
 
 export const Home = () => {
-  return (
-    <H.Content>
-        <H.Info>
-            <h1>Deixe sua avaliação</h1>
-            <p>Ficamos felizes com a sua sessão, utilize o formulário abaixo para avaliar o produto</p>
-        </H.Info>
+    return (
+        <H.Content>
+            <H.Info>
+                <h1>Deixe sua avaliação</h1>
+                <p>Ficamos felizes com a sua sessão, utilize o formulário abaixo para avaliar o produto</p>
+            </H.Info>
 
-        <H.FormContainer>
-            <H.Steps>
-                <H.Step className='active'>
-                    <FaUser  className='icon'/>
-                    <p>Identificação</p>
-                </H.Step>
-                <H.Step>
-                    <FaRegStar className='icon star'/>
-                    <p>Avaliação</p>
-                </H.Step>
-                <H.Step>
-                    <BsSend className='icon'/>
-                    <p>Envio</p>
-                </H.Step>
-            </H.Steps>
-        </H.FormContainer>
-    </H.Content>
-  )
+            <H.FormContainer>
+
+                <H.Steps>
+                    <H.Step className='active'>
+                        <FaUser className='icon' />
+                        <p>Identificação</p>
+                    </H.Step>
+                    <H.Step>
+                        <FaRegStar className='icon star' />
+                        <p>Avaliação</p>
+                    </H.Step>
+                    <H.Step>
+                        <BsSend className='icon' />
+                        <p>Envio</p>
+                    </H.Step>
+                </H.Steps>
+
+                <H.inputContainer>
+                    <H.labelContent>
+                        <label htmlFor="">Nome:</label>
+                        <input type="text" name="name" id="name" />
+                    </H.labelContent>
+
+                    <H.labelContent>
+                        <label htmlFor="">Email:</label>
+                        <input type="email" name="email" id="email" />
+                    </H.labelContent>
+
+                    <H.checkContent>
+                        <input type="checkbox" name="check" id="check" />
+                        <label htmlFor="check">Desejo permanecer anônimo</label>
+                    </H.checkContent>
+                </H.inputContainer>
+
+                <H.buttonContent>
+                    <H.buttonAvancar>Avançar</H.buttonAvancar>
+                </H.buttonContent>
+            </H.FormContainer>
+        </H.Content>
+    )
 }
--- frontend/src/Pages/Home/Styles.jsx
+++ frontend/src/Pages/Home/Styles.jsx
@@ -42,6 +42,8 @@ export const FormContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    gap: 30px;
 `
 
 export const Steps = styled.div`
@@ -93,5 +95,79 @@ export const Step = styled.div`
 
     p {
        height: 20px;
+    }
+`
+// InputContainer
+
+export const inputContainer = styled.div`
+    width: 800px;
+    height: 197px;
+    border: 1px solid #DDDDDD;
+    border-radius: 9px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 15px;
+`
+
+export const labelContent = styled.div`
+    width: 400px;
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+        background-color: #eee;
+        border: 1px solid #eee;
+        outline: 1px solid #ddd;
+        height: 60px;
+        margin-bottom: 5px;
+    }
+`
+
+export const checkContent = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 30px;
+    width: 400px;
+    gap: 5px;
+    
+    label {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input[type="checkbox"] {
+        width: 15px;    
+        cursor: pointer;
+    }
+`
+
+// Button
+
+export const buttonContent = styled.div`
+    width: 800px;
+    height: 44px;
+    display: flex;
+    justify-content: flex-end;
+`
+
+export const buttonAvancar = styled.button`
+    width: 121px;
+    height: 44px;
+    background-color: #457B9D;
+    color: #fff;
+    text-align: center;
+    cursor: pointer;
+    border: 1px solid #457B9D;
+    border-radius: 5px;
+    font-size: 0.9rem;
+    font-weight: bold;    
+    transition: transform 0.1s ease-in-out;
+
+    &:active {
+        transform: scale(0.97);
     }
 `
