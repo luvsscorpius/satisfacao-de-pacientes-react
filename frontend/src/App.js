09387904f28038595e730010634e75f35e6661a5
and <andersonvittor1801@gmail.com>
2024-09-15 10:44:12 -0300

    feat(frontend): creating globalstyle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Home } from './Pages/Home/Home';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Header } from './Components/Header/Header';
+import { GlobalStyle } from './Css/GlobalStyle';
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-    </Router>
+      <GlobalStyle/>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as H from './Styles'
+
+export const Header = () => {
+  return (
+    <H.Header>
+        Teste
+    </H.Header>
+  )
+}
