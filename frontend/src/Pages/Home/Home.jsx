47891f94d190624ac201977425c102fe71f7cf70
and <andersonvittor1801@gmail.com>
2024-09-17 21:00:52 -0300

    feat(frontend): implementing logic to see if user will send info or not

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from 'react'
 import * as H from './Styles'
 import { useNavigate } from 'react-router-dom';
-import {Info} from '../../Components/Info/Info';
-import {Steps} from '../../Components/Steps/Steps';
+import { Info } from '../../Components/Info/Info';
+import { Steps } from '../../Components/Steps/Steps';
 import { Context } from '../../Context/Context';
 
 export const Home = () => {
-    const {userInfo, setUserInfo, isChecked, setIsChecked, isRequired, setIsRequired} = useContext(Context)
+    const { userInfo, setUserInfo, isChecked, setIsChecked, isRequired, setIsRequired } = useContext(Context)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
 
     const navigate = useNavigate()
 
@@ -14,13 +16,17 @@ export const Home = () => {
         e.preventDefault()
     }
 
-    console.log(isChecked)
-
+    // Usando essa função para ver se o checkbox esta checado, se sim deixamos a pessoa passar para a próxima etapa do formulário
     const checkIsChecked = () => {
         if (isChecked === true) {
-            console.log(isChecked)
             setIsRequired(prevState => !prevState)
             navigate("/avalie")
+        }
+
+        if (name && email) {
+            // Enviando as informações
+            setUserInfo({ name, email, isChecked })
+            navigate('/avalie')
         }
     }
 
@@ -30,25 +36,25 @@ export const Home = () => {
 
     return (
         <H.Content>
-            <Info titulo="Deixe sua avaliação" p="Ficamos felizes com a sua sessão, utilize o formulário abaixo para avaliar o produto"/>
+            <Info titulo="Deixe sua avaliação" p="Ficamos felizes com a sua sessão, utilize o formulário abaixo para avaliar o produto" />
 
             <H.FormContainer onSubmit={handleClick}>
 
-                <Steps/>
+                <Steps />
 
                 <H.inputContainer>
                     <H.labelContent>
                         <label htmlFor="name">Nome:</label>
-                        <input type="text" name="name" id="name" required={isRequired}/>
+                        <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} required={isRequired} />
                     </H.labelContent>
 
                     <H.labelContent>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email" required={isRequired} />
+                        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required={isRequired} />
                     </H.labelContent>
 
                     <H.checkContent>
-                        <input type="checkbox" name="check" id="check" checked={isChecked} onChange={handleToggleCheck}/>
+                        <input type="checkbox" name="check" id="check" checked={isChecked} onChange={handleToggleCheck} />
                         <label htmlFor="check">Desejo permanecer anônimo</label>
                     </H.checkContent>
                 </H.inputContainer>
