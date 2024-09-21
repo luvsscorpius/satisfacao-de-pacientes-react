cc7a21c42319cc4de12627dbbe42514dc1c53640
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-09-21 17:38:00 -0300

    fix(frontend): changing function CheckIsChecked from onClick button to onSubmit form
    changing place where the function should be and adding setIsRequired on handleToggleCheck to let
    user pass to next page without sending its info

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -18,28 +18,32 @@ export const Home = () => {
     const checkIsChecked = (e) => {
         // adicionando e.preventDefault para arrumar o erro de Form submission canceled because the form is not connected
         e.preventDefault()
+
         if (isChecked === true) {
-            setIsRequired(prevState => !prevState)
-            navigate("/avaliacao")
+            setUserInfo({name: "", email: "", checkIsChecked})
             changeStep(currentStep + 1, e)
+            navigate("/avaliacao")
         }
 
         if (name && email) {
             // Enviando as informações
             setUserInfo({ name, email, isChecked })
+            changeStep(currentStep + 1, e)
             navigate('/avaliacao')
         }
     }
 
     const handleToggleCheck = () => {
         setIsChecked(prevState => !prevState)
+        // Usando setIsRequired para fazer o toggle de true/false ou vise versa para corrigir o erro de quando colocar o checkIsChecked no onSubmit ele permitir passar para a proxima pagina sendo anonimo
+        setIsRequired(prevState => !prevState)
     }
 
     return (
         <H.Content>
             <Info titulo="Deixe sua avaliação" p="Ficamos felizes com a sua sessão, utilize o formulário abaixo para avaliar o produto" />
 
-            <H.FormContainer >
+            <H.FormContainer onSubmit={(e) => checkIsChecked(e)} >
 
                 <Steps currentStep={currentStep} />
 
@@ -61,7 +65,7 @@ export const Home = () => {
                 </H.inputContainer>
 
                 <H.buttonContent>
-                    <H.buttonAvancar type='submit' onClick={(e) => checkIsChecked(e)}>Avançar</H.buttonAvancar>
+                    <H.buttonAvancar type='submit'>Avançar</H.buttonAvancar>
                 </H.buttonContent>
             </H.FormContainer>
         </H.Content>
