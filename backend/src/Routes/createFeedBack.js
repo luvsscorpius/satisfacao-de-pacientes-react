5db7993ae335dcaa2a6d1171e9dd5d438465a437
and <andersonvittor1801@gmail.com>
2024-09-25 19:06:07 -0300

    feat(backend): creating createFeedBack route

---
+++ backend/src/Routes/createFeedBack.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const Mongo = require('../config/db')
+
+router.put(('/info'), async (req, res) => {
+    try {
+        const {user} = req.params
+        console.log(user)
+    } catch(error) {
+        console.error(error)
+    }
+})
+
+module.exports = router
--- backend/src/Server.js
+++ backend/src/Server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Importando as rotas
+const createFeedBack = require('./Routes/createFeedBack')
+
+// Usando as rotas
+app.use('/createFeedBack', createFeedBack)
+
 app.listen(port, () => {
     console.log('Servidor rodando na porta', port)
 })
