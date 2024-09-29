0686a6e69ef4de98eeff9bf4df70a895f00e8b06
and <andersonvittor1801@gmail.com>
2024-09-29 19:26:21 -0300

    feat(frontend): styling react admin header

--- frontend/src/Pages/Adm/Adm.jsx
+++ frontend/src/Pages/Adm/Adm.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import * as Home from '../Home/Styles'
-import {Admin, Resource} from 'react-admin'
+import {Admin, Layout, Resource} from 'react-admin'
 import {dataProvider} from './DataProvider'
 import { UserList } from '../../Components/UserList/UserList'
+import { AppBar } from 'react-admin';
+import { styled } from '@mui/material/styles';
+
+
+// Estilizando o AppBar usando Material-UI
+const CustomAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: '#457B9D',
+  color: '#FFFFFF',         
+  boxShadow: 'none',        
+  padding: '0 20px',         
+  height: '50px',    
+  position: 'absolute',
+  display: 'flex;',
+  justifyContent: 'center'
+}));
+
+// Criando um layout customizado com o AppBar
+const CustomLayout = (props) => (
+  <Layout {...props} appBar={CustomAppBar} />
+);
 
 export const Adm = () => {
   return (
-    <Admin dataProvider={dataProvider} basename="/adm" >
+    <Admin layout={CustomLayout} dataProvider={dataProvider} basename="/adm" >
       <Resource name='users' list={UserList} />
     </Admin>
   )
