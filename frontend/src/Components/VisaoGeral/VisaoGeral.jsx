5d265775df2db42ab11498eea001681cf026e651
and <andersonvittor1801@gmail.com>
2024-10-03 10:47:38 -0300

    feat(frontend): creating totalfeedbacks on visao geral

--- frontend/src/Components/VisaoGeral/VisaoGeral.jsx
+++ frontend/src/Components/VisaoGeral/VisaoGeral.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../Context/Context'
 
 export const VisaoGeral = () => {
+    const {allFeedbacks} = useContext(Context)
+
+    // Separando os feedbacks
+    const anoFeedbacks = allFeedbacks.isAnonymousFeedbacks || []
+    const nonAnoFeedbacks = allFeedbacks.nonAnonymousFeedbacks || []
+
+    // Variavel para contar os feedbacks
+    let totalFeedbacks = 0
+
+    // Varrendo os feedbacks anonimos
+    anoFeedbacks.forEach(feedback => {
+        if (feedback) {
+            totalFeedbacks += 1
+        }
+
+        return totalFeedbacks
+    })
+
+    // Varrendo os feedbacks nao anonimos
+    nonAnoFeedbacks.forEach(feedback => {
+        if (feedback) {
+            totalFeedbacks += 1
+        }
+
+        return totalFeedbacks
+    })
+
+    console.log(totalFeedbacks)
+
   return (
-    <div>VisaoGeral</div>
+    <div style={{textAlign: 'center'}}>
+        <h3>Quantidade de avaliações</h3>
+        <h2>{totalFeedbacks}</h2>
+    </div>
   )
 }
