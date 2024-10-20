7ab7e5f2abda63f5e6fa75329cd7fe4f130e6322
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2024-10-20 11:24:23 -0300

    feat(frontend): adding @media to toast-container

--- frontend/src/Css/GlobalStyle.jsx
+++ frontend/src/Css/GlobalStyle.jsx
@@ -10,4 +10,14 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
+
+    @media (max-width: 768px) {
+     .toast-container {
+        .Toastify__toast {
+            width: 90%;
+            margin: 0 auto;
+            border-radius: 5px;
+            margin-top: 5px;
+        }
+    }
 `;
